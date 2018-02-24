87f6791a0e0102b4b7c0e9a927df94bf9f00f5ad
Jason Fry <funkytomato@gmail.com>
2018-02-24 18:01:44 +0000

    Initial release

---
+++ ECGameFramework/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 23/02/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: Properties
+    
+    var window: UIWindow?
+}
+
