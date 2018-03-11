23ef61988c7ea5f26562026693a1adac233a732a
Jason Fry <funkytomato@gmail.com>
2018-03-11 01:19:42 +0000

    Initial release

---
+++ ECGameFramework/Components/SpriteComponent.swift
@@ -0,0 +1,37 @@
+//
+//  SpriteComponent.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 06/03/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class SpriteComponent: GKComponent
+{
+    // MARK:- Properties
+    
+    //The SpriteNode
+    let node : SKSpriteNode
+
+    
+    //MARK:- Initialisers
+    init(texture: SKTexture, textureSize: CGSize)
+    {
+        node = SKSpriteNode(texture: texture, size: textureSize)
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK:- GKComponent
+    
+    override func update(deltaTime: TimeInterval)
+    {
+        super.update(deltaTime: deltaTime)
+    }
+}
