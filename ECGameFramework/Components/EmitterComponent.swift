a99486861237ecc8bc74ee0583c88cc619f6a6eb
Jason Fry <funkytomato@gmail.com>
2018-03-11 01:20:24 +0000

    Initial release

---
+++ ECGameFramework/Components/EmitterComponent.swift
@@ -0,0 +1,47 @@
+//
+//  EmitterComponent.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 10/03/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+class EmitterComponent: GKComponent
+{
+    // MARK :- Properties
+    
+    //The Emitter Node
+    let node : SKEmitterNode
+    
+    var defaultParticleBirthRate : Float
+    
+    
+    // MARK :- Initialisers
+    init(particleName: String)
+    {
+        node = SKEmitterNode(fileNamed: particleName)!
+        
+        defaultParticleBirthRate = Float(node.particleBirthRate)
+        node.position = CGPoint(x: 0, y: -60)
+        node.particleBirthRate = 10
+        node.particleLifetime = 100
+        node.zPosition = 1
+        
+        print((node.description))
+        
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func update(deltaTime: TimeInterval)
+    {
+        super.update(deltaTime: deltaTime)
+    }
+}
