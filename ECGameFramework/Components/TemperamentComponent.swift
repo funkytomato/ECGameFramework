21180a50f413bd7dc0718da8b2b2a4bd3e59f5a1
Jason Fry <funkytomato@gmail.com>
2018-04-17 16:36:47 +0100

    Add a TemperamentComponent.  Effects how an entity reacts in various situations

---
+++ ECGameFramework/Components/TemperamentComponent.swift
@@ -0,0 +1,111 @@
+//
+//  TemperamentComponent.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 16/04/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+//     Abstract:
+//  A `GKComponent` that provides a `GKStateMachine` for entities to use in determining their temperament.
+
+import SpriteKit
+import GameplayKit
+
+class TemperamentComponent: GKComponent
+{
+    
+    // MARK: Properties
+    
+    let stateMachine: GKStateMachine
+    
+    let initialStateClass: AnyClass
+    
+    // MARK: Initializers
+    
+    init(states: [GKState])
+    {
+        stateMachine = GKStateMachine(states: states)
+        let firstState = states.first!
+        initialStateClass = type(of: firstState)
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: GKComponent Life Cycle
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        stateMachine.update(deltaTime: seconds)
+    }
+    
+    // MARK: Actions
+    
+    func enterInitialState()
+    {
+        stateMachine.enter(initialStateClass)
+    }
+    
+    /*
+     Convenience functions
+    */
+    
+    /*
+    Increase the temperament of the entity
+    */
+    func increaseTemperament()
+    {
+        let currentTemperament = stateMachine.currentState
+        print("currentTemperament:\(currentTemperament.debugDescription)")
+        
+        switch currentTemperament
+        {
+        case is ScaredState:
+            stateMachine.enter(CalmState.self)
+            
+        case is CalmState:
+            stateMachine.enter(AngryState.self)
+            
+        case is AngryState:
+            stateMachine.enter(ViolentState.self)
+            
+        case is ViolentState:
+            stateMachine.enter(ViolentState.self)
+            
+        default:
+            stateMachine.enter(CalmState.self)
+        }
+    }
+    
+    //Decrease the temperament of the entity
+    func decreaseTemperament()
+    {
+        let currentTemperament = stateMachine.currentState
+        
+        switch currentTemperament
+        {
+        case is ScaredState:
+            stateMachine.enter(ScaredState.self)
+            
+        case is CalmState:
+            stateMachine.enter(ScaredState.self)
+            
+        case is AngryState:
+            stateMachine.enter(CalmState.self)
+            
+        case is ViolentState:
+            stateMachine.enter(AngryState.self)
+            
+        default:
+            stateMachine.enter(CalmState.self)
+        }
+    }
+    
+    
+    
+}
