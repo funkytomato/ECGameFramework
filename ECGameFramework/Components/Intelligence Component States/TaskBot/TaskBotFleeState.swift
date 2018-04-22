0995133b97a25d16ba1c14c82d4756d694365d1c
Jason Fry <funkytomato@gmail.com>
2018-04-22 21:02:45 +0100

    Initial release

---
+++ ECGameFramework/Components/Intelligence Component States/TaskBot/TaskBotFleeState.swift
@@ -0,0 +1,111 @@
+/*
+//
+//  TaskBotFleeState.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 19/04/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+The state of a `TaskBot` when actively fleeing from another another `TaskBot`.
+
+*/
+
+import SpriteKit
+import GameplayKit
+
+class TaskBotFleeState: GKState
+{
+    // MARK: Properties
+    
+    unowned var entity: TaskBot
+    
+    /// The distance to the current target on the previous update loop.
+    var lastDistanceToTarget: Float = 0
+    
+    /// The `MovementComponent` associated with the `entity`.
+    var movementComponent: MovementComponent
+    {
+        guard let movementComponent = entity.component(ofType: MovementComponent.self) else { fatalError("A TaskBot FleeState's entity must have a MovementComponent.") }
+        return movementComponent
+    }
+    
+    /// The `PhysicsComponent` associated with the `entity`.
+    var physicsComponent: PhysicsComponent
+    {
+        guard let physicsComponent = entity.component(ofType: PhysicsComponent.self) else { fatalError("A TaskBot FleeState's entity must have a PhysicsComponent.") }
+        return physicsComponent
+    }
+    
+    
+    // MARK: Initializers
+    
+    required init(entity: TaskBot)
+    {
+        self.entity = entity
+    }
+    
+    // MARK: GKState Life Cycle
+    
+    override func didEnter(from previousState: GKState?)
+    {
+        super.didEnter(from: previousState)
+        
+     }
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        stateMachine?.enter(TaskBotAgentControlledState.self)
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is TaskBotAgentControlledState.Type, is TaskBotZappedState.Type:
+            return true
+            
+        default:
+            return false
+        }
+    }
+    
+    override func willExit(to nextState: GKState)
+    {
+        super.willExit(to: nextState)
+        
+        // `movementComponent` is a computed property. Declare a local version so we don't compute it multiple times.
+        let movementComponent = self.movementComponent
+        
+        // Stop the `ManBot`'s movement and restore its standard movement speed.
+        movementComponent.nextRotation = nil
+        movementComponent.nextTranslation = nil
+        movementComponent.movementSpeed /= GameplayConfiguration.ManBot.movementSpeedMultiplierWhenAttacking
+        movementComponent.angularSpeed /= GameplayConfiguration.ManBot.angularSpeedMultiplierWhenAttacking
+    }
+    
+    // MARK: Convenience
+    
+    func applyDamageToEntity(entity: GKEntity)
+    {
+        if let playerBot = entity as? PlayerBot, let chargeComponent = playerBot.component(ofType: ChargeComponent.self), !playerBot.isPoweredDown
+        {
+            // If the other entity is a `PlayerBot` that isn't powered down, reduce its charge.
+            chargeComponent.loseCharge(chargeToLose: GameplayConfiguration.ManBot.chargeLossPerContact)
+        }
+        else if let taskBot = entity as? TaskBot, taskBot.isGood
+        {
+            //Move state to being Arrested
+            guard let intelligenceComponent = entity.component(ofType: IntelligenceComponent.self) else { return }
+            intelligenceComponent.stateMachine.enter(BeingArrestedState.self)
+            
+            
+            // If the other entity is a good `TaskBot`, turn it bad.
+            //taskBot.isGood = false
+        }
+    }
+    
+}
