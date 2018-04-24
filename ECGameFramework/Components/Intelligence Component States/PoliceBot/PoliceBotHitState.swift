618175b7c6266b4079947e5316979ea19835c07d
Jason Fry <funkytomato@gmail.com>
2018-04-24 15:17:56 +0100

    ProtestorBotHitState

---
+++ ECGameFramework/Components/Intelligence Component States/PoliceBot/PoliceBotHitState.swift
@@ -0,0 +1,86 @@
+/*
+//
+//  PoliceBotHitState.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 24/04/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+A state used to represent the player when hit by a `TaskBot` attack.
+*/
+
+import SpriteKit
+import GameplayKit
+
+class PoliceBotHitState: GKState
+{
+    // MARK: Properties
+    
+    unowned var entity: PoliceBot
+    
+    /// The amount of time the `PlayerBot` has been in the "hit" state.
+    var elapsedTime: TimeInterval = 0.0
+    
+    /// The `AnimationComponent` associated with the `entity`.
+    var animationComponent: AnimationComponent
+    {
+        guard let animationComponent = entity.component(ofType: AnimationComponent.self) else { fatalError("A PoliceBotHitState's entity must have an AnimationComponent.") }
+        return animationComponent
+    }
+    
+    // MARK: Initializers
+    
+    required init(entity: PoliceBot)
+    {
+        self.entity = entity
+    }
+    
+    // MARK: GKState Life Cycle
+    
+    override func didEnter(from previousState: GKState?)
+    {
+        super.didEnter(from: previousState)
+        
+        // Reset the elapsed "hit" duration on entering this state.
+        elapsedTime = 0.0
+        
+        // Request the "hit" animation for this `PlayerBot`.
+        animationComponent.requestedAnimationState = .hit
+    }
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        // Update the amount of time the `PlayerBot` has been in the "hit" state.
+        elapsedTime += seconds
+        
+        // When the `PlayerBot` has been in this state for long enough, transition to the appropriate next state.
+        if elapsedTime >= GameplayConfiguration.PoliceBot.hitStateDuration
+        {
+            if entity.isPoweredDown
+            {
+                stateMachine?.enter(PoliceBotRechargingState.self)
+            }
+            else
+            {
+                stateMachine?.enter(TaskBotAgentControlledState.self)
+            }
+        }
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is TaskBotAgentControlledState.Type, is PoliceBotRechargingState.Type:
+            return true
+            
+        default:
+            return false
+        }
+    }
+}
+
