a5b4afea9d9bd9e82cef25983b8f7be883886e22
Jason Fry <funkytomato@gmail.com>
2018-04-25 21:55:20 +0100

    Renamed isGood to isProtestor

--- ECGameFramework/Entities/PoliceBot.swift
+++ ECGameFramework/Entities/PoliceBot.swift
@@ -309,9 +309,9 @@ class PoliceBot: TaskBot, ChargeComponentDelegate, ResourceLoadableType
     {
         guard let intelligenceComponent = component(ofType: IntelligenceComponent.self) else { return }
         
-        isGood = !chargeComponent.hasCharge
+        isProtestor = !chargeComponent.hasCharge
         
-        if !isGood
+        if !isProtestor
         {
             intelligenceComponent.stateMachine.enter(TaskBotZappedState.self)
         }
