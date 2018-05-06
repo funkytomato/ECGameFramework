eecd262a0cd84362cc15ab5844a68c61e26fefa0
Jason Fry <funkytomato@gmail.com>
2018-05-06 15:52:34 +0100

    Initial release

---
+++ ECGameFramework/Components/ResistanceComponent State/ResistanceCoolingState.swift
@@ -0,0 +1,77 @@
+/*
+//
+//  ResistanceCoolingState.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 06/05/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+The state the beam enters when it overheats from being used for too long.
+*/
+
+import SpriteKit
+import GameplayKit
+
+class ResistanceCoolingState: GKState
+{
+    // MARK: Properties
+    
+    unowned var resistanceComponent: ResistanceComponent
+    
+    /// The amount of time the beam has been cooling down.
+    var elapsedTime: TimeInterval = 0.0
+    
+    // MARK: Initializers
+    
+    required init(resistanceComponent: ResistanceComponent)
+    {
+        self.resistanceComponent = resistanceComponent
+    }
+    
+    // MARK: GKState life cycle
+    
+    override func didEnter(from previousState: GKState?)
+    {
+        super.didEnter(from: previousState)
+        
+        elapsedTime = 0.0
+    }
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        elapsedTime += seconds
+        
+        // If the beam has spent long enough cooling down, enter `BeamIdleState`.
+        if elapsedTime >= GameplayConfiguration.Beam.coolDownDuration
+        {
+            stateMachine?.enter(ResistanceIdleState.self)
+        }
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is ResistanceIdleState.Type, is ResistanceHitState.Type:
+            return true
+            
+        default:
+            return false
+        }
+    }
+    
+    override func willExit(to nextState: GKState)
+    {
+        super.willExit(to: nextState)
+        
+/*        if let protestorBot = resistanceComponent.entity as? PlayerBot
+        {
+            resistanceComponent.beamNode.update(withBeamState: nextState, source: playerBot)
+        }
+ */
+    }
+}
---
+++ ECGameFramework/Components/ResistanceComponent State/ResistanceIdleState.swift
@@ -0,0 +1,64 @@
+/*
+//
+//  ResistanceIdleState.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 06/05/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+The state of the `PlayerBot`'s beam when not in use.
+*/
+
+import SpriteKit
+import GameplayKit
+
+class ResistanceIdleState: GKState
+{
+    // MARK: Properties
+    
+    unowned var resistanceComponent: ResistanceComponent
+    
+    // MARK: Initializers
+    
+    required init(resistanceComponent: ResistanceComponent)
+    {
+        self.resistanceComponent = resistanceComponent
+    }
+    
+    // MARK: GKState life cycle
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        // If the resistance has been triggered, enter `ResistanceHitState`.
+        if resistanceComponent.isTriggered
+        {
+            stateMachine?.enter(ResistanceHitState.self)
+        }
+        else
+        {
+            if !resistanceComponent.isFullyResistanced
+            {
+            
+                // Add resistance to the `ProtestorBot`.
+                let amountToRecharge = GameplayConfiguration.ProtestorBot.rechargeAmountPerSecond * seconds
+                resistanceComponent.addResistance(resistanceToAdd: amountToRecharge)
+            }
+        }
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is ResistanceHitState.Type:
+            return true
+            
+        default:
+            return false
+        }
+    }
+}
