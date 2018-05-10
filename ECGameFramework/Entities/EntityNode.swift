7251de75057501abc233d455739642c31bc7455f
Jason Fry <funkytomato@gmail.com>
2018-05-11 00:25:20 +0100

    Trying to implement touch on taskbots

---
+++ ECGameFramework/Entities/EntityNode.swift
@@ -0,0 +1,14 @@
+//
+//  EntityNode.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 10/05/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+import GameplayKit
+
+class EntityNode: SKSpriteNode
+{
+    weak var gkEntity: GKEntity!
+}
