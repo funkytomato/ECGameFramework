797317058218af425e4700b992236d506ec3118f
Jason Fry <funkytomato@gmail.com>
2018-05-24 21:09:50 +0100

    Initial release

---
+++ ECGameFramework/Components/Intelligence Component States/CriminalBot/InciteState.swift
@@ -0,0 +1,88 @@
+/*
+//
+//  InciteState.swift
+//  ECGameFramework
+//
+//  Created by Jason Fry on 24/05/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+ The Criminal is currently inciting trouble in the crowd.
+ The Crim wanders around the scene, and periodically becomes active and whoever they become in contact with during this time becomes influenced.
+*/
+
+import SpriteKit
+import GameplayKit
+
+class InciteState: GKState
+{
+    // MARK:- Properties
+    unowned var entity: CriminalBot
+    
+    // The amount of time the 'ManBot' has been in its "Detained" state
+    var elapsedTime: TimeInterval = 0.0
+    
+    
+    /// The `AnimationComponent` associated with the `entity`.
+    var animationComponent: AnimationComponent
+    {
+        guard let animationComponent = entity.component(ofType: AnimationComponent.self) else { fatalError("A LootState's entity must have an AnimationComponent.") }
+        return animationComponent
+    }
+    
+    /// The `TemperamentComponent` associated with the `entity`.
+    var temperamentComponent: TemperamentComponent
+    {
+        guard let temperamentComponent = entity.component(ofType: TemperamentComponent.self) else { fatalError("A LootState's entity must have an TemperamentComponent.") }
+        return temperamentComponent
+    }
+    
+    //MARK:- Initializers
+    required init(entity: CriminalBot)
+    {
+        self.entity = entity
+    }
+    
+    
+    //MARK:- GKState Life Cycle
+    override func didEnter(from previousState: GKState?)
+    {
+        super.didEnter(from: previousState)
+        
+        //Reset the tracking of how long the 'ManBot' has been in "Detained" state
+        elapsedTime = 0.0
+        
+        //Request the "detained animation for this state's 'ProtestorBot'
+        animationComponent.requestedAnimationState = .arrested
+        
+        //temperamentComponent.stateMachine.enter(SubduedState.self)
+        //entity.isActive = false
+    }
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        
+        
+        elapsedTime += seconds
+        
+        /*
+         If the arrested manbot reaches the meatwagon pointer, move to detained state
+         */
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+            case is TaskBotAgentControlledState.Type:
+                return true
+            
+            default:
+                return false
+        }
+    }
+}
+
