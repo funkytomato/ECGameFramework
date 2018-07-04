56c54e5a0aa55663c44c8f675946b32c093370df
Jason Fry <funkytomato@gmail.com>
2018-07-04 23:36:06 +0100

    Change to greater than 100

--- ECGameFramework/Entities/ProtestorBot.swift
+++ ECGameFramework/Entities/ProtestorBot.swift
@@ -453,7 +453,7 @@ class ProtestorBot: TaskBot, HealthComponentDelegate, ResistanceComponentDelegat
     {
         print("Appetite Component Add Appetite")
         
-        if appetiteComponent.appetite == 100.0
+        if appetiteComponent.appetite >= 100.0
         {
             //Protestor wants to be a product
             appetiteComponent.isTriggered = true
