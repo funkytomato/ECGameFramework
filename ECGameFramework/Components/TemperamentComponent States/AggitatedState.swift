7baac3d40092a08f150b828631a1200674026dd8
funkytomato <funkytomato@gmail.com>
2018-07-20 15:03:45 +0100

    Initial release

---
+++ ECGameFramework/Components/TemperamentComponent States/AggitatedState.swift
@@ -0,0 +1,86 @@
+/*
+//
+//  AggitatedState.swift
+//  ECGameFramework
+//
+//  Created by Spaceman on 20/07/2018.
+//  Copyright © 2018 Jason Fry. All rights reserved.
+//
+
+Abstract:
+The state `ProtestorBot`s are in immediately after being arrested.
+
+Remove all behavour from bot and attach to the arresting entity.
+*/
+
+import SpriteKit
+import GameplayKit
+
+class AggitatedState: GKState
+{
+    // MARK:- Properties
+    unowned var entity: TaskBot
+    
+    //The amount of time the 'ProtestorBot' has been in its "AggitatedState" state
+    var elapsedTime: TimeInterval = 0.0
+    
+    //The MeatWagon location
+    let meatWagonCoordinate = float2(x: 0.0, y: 0.0)
+    
+    /// The `SpriteComponent` associated with the `entity`.
+    var spriteComponent: SpriteComponent
+    {
+        guard let spriteComponent = entity.component(ofType: SpriteComponent.self) else { fatalError("An entity's AggitatedState must have an AnimationComponent.") }
+        return spriteComponent
+    }
+    
+    //MARK:- Initializers
+    required init(entity: TaskBot)
+    {
+        self.entity = entity
+    }
+    
+    deinit {
+        //        print("Deallocating AggitatedState")
+    }
+    
+    
+    //MARK:- GKState Life Cycle
+    override func didEnter(from previousState: GKState?)
+    {
+        super.didEnter(from: previousState)
+        
+        //Reset the tracking of how long the 'ProtestorBot' has been in "AggitatedState" state
+        elapsedTime = 0.0
+        
+        //Change the colour of the sprite to show violent
+        spriteComponent.changeColour(colour: SKColor.red)
+        
+        entity.isViolent = false
+        
+        entity.isScared = false
+    }
+    
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+        
+        elapsedTime += seconds
+        
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is ScaredState.Type, is CalmState.Type, is AngryState.Type, is SubduedState.Type:
+            return true
+            
+        default:
+            return false
+        }
+    }
+    
+    // MARK: Convenience
+    
+}
