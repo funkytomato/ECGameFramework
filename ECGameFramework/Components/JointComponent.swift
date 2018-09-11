fb15a72a13c28c1e3c5a370a83bdb97ddcfa84e8
Jason Fry <funkytomato@gmail.com>
2018-09-11 21:46:17 +0100

    Fixed double creation of satelliteNode on main scene. Cleaned up redundant code. Implemented different physic joint types.

--- ECGameFramework/Components/JointComponent.swift
+++ ECGameFramework/Components/JointComponent.swift
@@ -15,40 +15,107 @@ A TaskBot can only incite others periodically and can only influence others near
 import SpriteKit
 import GameplayKit
 
+let kPinnedNode = "pinned"                  //this will be the policeman A
+let kSatelliteNode = "satellite"            //this will be the policeman b
+let kJointOutlineNode = "jointoutline"
+let kPinDotNode = "pindot"
+let kSatelliteDotNode = "satellitedot"
+let kLineNode = "lineNode"
+
+enum JointLabels : String
+{
+    case Pin = "Pin"
+    case Fixed = "Fixed"
+    case Spring = "Spring"
+    case Sliding = "Sliding"
+    case Limit = "Limit"
+}
+
+class JointOutline : SKNode
+{
+    
+    class func create(_ scene:SKScene) -> SKNode
+    {
+        let parent = JointOutline()
+        parent.name = kJointOutlineNode
+        
+        let pinDot = SKShapeNode(circleOfRadius: 6)
+        pinDot.fillColor = UIColor.red
+        pinDot.name = kPinDotNode
+        
+        let satelliteDot = SKShapeNode(circleOfRadius: 6)
+        satelliteDot.fillColor = UIColor.red
+        satelliteDot.name = kSatelliteDotNode
+        
+        let lineNode = SKShapeNode()
+        lineNode.name = kLineNode
+        lineNode.strokeColor = UIColor.red
+        lineNode.lineWidth = 3.0
+        
+        scene.addChild(parent)
+        parent.updateOutline()
+        
+        return parent
+    }
+    
+    func updateOutline()
+    {
+        guard let renderComponent = entity?.component(ofType: RenderComponent.self) else { return }
+        guard let jointComponent = entity?.component(ofType: JointComponent.self) else { return }
+        
+//         Make the pin the Policeman A node
+        let pin = renderComponent.node
+        
+//         Make the satellite the Policeman B node
+        let satellite = jointComponent.entityB?.component(ofType: RenderComponent.self)?.node
+        let pinDot = childNode(withName: kPinDotNode)
+        let satelliteDot = childNode(withName: kSatelliteDotNode)
+        let line = childNode(withName: kLineNode) as! SKShapeNode
+        
+        pinDot?.position = (scene?.convert(CGPoint.zero, from: pin))!
+        satelliteDot?.position = (scene?.convert(CGPoint.zero, from: satellite!))!
+        
+        let bez = UIBezierPath()
+        bez.move(to: (pinDot?.position)!)
+        bez.addLine(to: (satelliteDot?.position)!)
+        line.path = bez.cgPath
+        
+    }
+    
+}
+
+
 class JointComponent: GKComponent
 {
     
-//    var entity: GKEntity?
-    
-    // MARK: Types
-    let kPinnedNode = "pinned"
-    let kSatelliteNode = "satellite"
-    let kJointOutlineNode = "jointoutline"
-    let kPinDotNode = "pindot"
-    let kSatelliteDotNode = "satellitedot"
-    let kLineNode = "lineNode"
-    
-    enum JointLabels : String
-    {
-        case Pin = "Pin"
-        case Fixed = "Fixed"
-        case Spring = "Spring"
-        case Sliding = "Sliding"
-        case Limit = "Limit"
-    }
-    
-    var thisJoint = SKNode()
     var entityB : TaskBot?
     var pinDot : SKShapeNode?
+    var pinned : SKSpriteNode?
     var satelliteDot : SKShapeNode?
+    var satellite : SKSpriteNode?
     var lineNode : SKShapeNode?
+    
+
+    var midPt:CGPoint {
+        return (pinned?.anchorPoint)!
+    }
+    
+    var pinnedHome:CGPoint {
+        var mid = midPt
+        mid.y += 120
+        return mid
+    }
+    
+    var satelliteHome:CGPoint {
+        var mid = midPt
+        mid.y -= 120
+        return mid
+    }
     
     // MARK: Properties
     
     /// Set to `true` whenever the player is holding down the attack button.
     var isTriggered = false
-    
-//    var physicsJoint : SKPhysicsJointLimit?
     
     
     /// The `RenderComponent' for this component's 'entity'.
@@ -63,39 +130,41 @@ class JointComponent: GKComponent
     
     init(entity: TaskBot)
     {
-//        var parent = JointOutline()
-//        parent.name = kJointOutlineNode
-//        self.entity = entity
+        var parent = JointOutline()
+        parent.name = kJointOutlineNode
+
         self.entityB = nil
         
         super.init()
         
-//        let pinDot = SKShapeNode(circleOfRadius: 6)
-//        pinDot.fillColor = UIColor.green
-//        pinDot.name = kPinDotNode
-//        pinDot.zPosition = 10
-//        self.pinDot = pinDot
-//
-//        let satelliteDot = SKShapeNode(circleOfRadius: 6)
-//        satelliteDot.fillColor = UIColor.red
-//        satelliteDot.name = kSatelliteDotNode
-//        satelliteDot.zPosition = 12
-//        self.satelliteDot = satelliteDot
-//
-//        let lineNode = SKShapeNode()
-//        lineNode.name = kLineNode
-//        lineNode.strokeColor = UIColor.red
-//        lineNode.lineWidth = 3.0
-//        self.lineNode = lineNode
-//
-//
-//        guard let renderComponent = entity.component(ofType: RenderComponent.self) else { return }
-//        renderComponent.node.addChild(pinDot)
-//        renderComponent.node.addChild(satelliteDot)
-//        renderComponent.node.addChild(lineNode)
+        let pinDot = SKShapeNode(circleOfRadius: 6)
+        pinDot.fillColor = UIColor.green
+        pinDot.name = kPinDotNode
+        pinDot.zPosition = 10
+        self.pinDot = pinDot
+
+        let satelliteDot = SKShapeNode(circleOfRadius: 6)
+        satelliteDot.fillColor = UIColor.red
+        satelliteDot.name = kSatelliteDotNode
+        satelliteDot.zPosition = 12
+        self.satelliteDot = satelliteDot
+        
+        let pinned = SKSpriteNode(color: UIColor.green, size: CGSize(width: 20,height: 20))
+        pinned.name = kPinnedNode
+        self.pinned = pinned
+        
+        let satellite = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 20,height: 20))
+        satellite.name = kSatelliteNode
+        self.satellite = satellite
+
         
         
 
+        let lineNode = SKShapeNode()
+        lineNode.name = kLineNode
+        lineNode.strokeColor = UIColor.red
+        lineNode.lineWidth = 3.0
+        self.lineNode = lineNode
      }
     
     required init?(coder aDecoder: NSCoder)
@@ -116,32 +185,26 @@ class JointComponent: GKComponent
         if isTriggered
         {
         
-////            var pin = renderComponent.node.childNode(withName: kPinnedNode)
-////            var satellite = renderComponent.node.childNode(withName: kSatelliteNode)
-//            var pinDot = renderComponent.node.childNode(withName: kPinDotNode)
-//            var satelliteDot = renderComponent.node.childNode(withName: kSatelliteDotNode)
-////            var line = renderComponent.node.childNode(withName: kLineNode) as! SKShapeNode
-//            
-//            
-//            guard let renderComponent = entity?.component(ofType: RenderComponent.self) else { return }
-//            pinDot!.position = renderComponent.node.scene!.convert(CGPoint(x: 50.0, y: 0.0), from: renderComponent.node.parent!)
-//
-////            pinDot!.position = renderComponent.node.scene!.convert(CGPoint.zero, from: renderComponent.node.parent!)
-//            
-////            guard let entityBRenderComponent = entityB?.component(ofType: RenderComponent.self) else { return }
-////            satelliteDot!.position = renderComponent.node.scene!.convert(CGPoint(x: -0.5, y: -0.5), from: entityBRenderComponent.node)
-////            satelliteDot!.position = renderComponent.node.scene!.convert(CGPoint.zero, from: entityBRenderComponent.node.parent!)
-//            
-////            print("entityA position: \(renderComponent.node.position), entityb position: \(entityBRenderComponent.node.position)")
-//            
-//            self.pinDot = pinDot as! SKShapeNode
-////            self.satelliteDot = satelliteDot as! SKShapeNode
-//            
-//            
-////            let bez = UIBezierPath()
-////            bez.move(to: pinDot!.position)
-////            bez.addLine(to: satelliteDot!.position)
-////            self.lineNode?.path = bez.cgPath
+            let jointOutline = renderComponent.node.scene!.childNode(withName: kJointOutlineNode) as! JointOutline
+            jointOutline.updateOutline()
+            
+//            if( alternating )
+//            {
+//                if let timeMark = lastTimeMark
+//                {
+//                    if( currentTime - timeMark > alternatingThreshold )
+//                    {
+//                        impulseVelocity = CGVector( dx: impulseVelocity.dx * -1.0, dy: impulseVelocity.dy * -1.0)
+//                        _doImpulse(impulseVelocity)
+//                        lastTimeMark = currentTime
+//                    }
+//                }
+//                else
+//                {
+//                    lastTimeMark = currentTime
+//                }
+//            }
+            
         }
     }
     
@@ -151,26 +214,117 @@ class JointComponent: GKComponent
     {
         self.entityB = targetEntity
         
-        guard let renderComponent = targetEntity.component(ofType: RenderComponent.self) else { return }
-//        satelliteDot?.position = renderComponent.node.scene!.convert(CGPoint.zero, from: renderComponent.node)
-        
-        let bodyA = entity!.component(ofType: RenderComponent.self)?.node.physicsBody
-        let bodyB = self.entityB!.component(ofType: RenderComponent.self)?.node.physicsBody
-        
-        let physicsJoint = SKPhysicsJointLimit.joint(withBodyA: bodyA!,
-                                                     bodyB: bodyB!,
-                                                     anchorA: CGPoint.zero,
-                                                     anchorB: CGPoint.zero)
-        
-        physicsJoint.maxLength = 10.0
-        
-        
-//        let physicsJoint = SKPhysicsJointFixed.joint(withBodyA: bodyA!, bodyB: bodyB!, anchor: CGPoint.zero)
+//        satellite!.position = (entityB?.component(ofType: RenderComponent)?.node.position)!
 
         
-        renderComponent.node.scene?.physicsWorld.add(physicsJoint)
+        renderComponent.node.addChild(pinned!)
+        renderComponent.node.addChild(pinDot!)
+        
+        
+        self.entityB?.component(ofType: RenderComponent.self)?.node.addChild(satellite!)
+        self.entityB?.component(ofType: RenderComponent.self)?.node.addChild(satelliteDot!)
+        
+        resetPhysicsBodies()
+        JointOutline.create(renderComponent.node.scene!)
+        makeJoint(JointLabels.Limit)
         
         self.isTriggered = true
+        
+        
+//        print("bodyAPosition: \(bodyAPosition), bodyBPosition: \(bodyBPosition)")
+    }
+    
+    func resetPhysicsBodies()
+    {
+    
+//        let pinned = self.pinned as? SKSpriteNode
+//        let satellite = self.satellite as? SKSpriteNode
+//        let pinned = renderComponent.node.scene!.childNode(withName: kPinnedNode) as! SKSpriteNode
+//        let satellite = renderComponent.node.scene!.childNode(withName: kSatelliteNode) as! SKSpriteNode
+        
+        // remember if the pinned body is currently dynamic
+        var dynamic = false
+        if let pbody = pinned!.physicsBody
+        {
+            dynamic = pbody.isDynamic
+        }
+        
+//        pinned!.physicsBody = SKPhysicsBody(rectangleOf: pinned!.size)
+        pinned!.physicsBody?.isDynamic = dynamic
+//        pinned!.physicsBody?.affectedByGravity = false
+        
+//        satellite!.physicsBody = SKPhysicsBody(rectangleOf: satellite!.size)
+//        satellite!.physicsBody?.isDynamic = true
+    }
+    
+    func resetNodes()
+    {
+        renderComponent.node.scene!.childNode(withName: kPinnedNode)?.position = pinnedHome
+        renderComponent.node.scene!.childNode(withName: kSatelliteNode)?.position = satelliteHome
+    }
+    
+    func makeJoint(_ name:JointLabels)
+    {
+        renderComponent.node.scene!.physicsWorld.removeAllJoints()
+        
+        resetNodes()
+        if( name != .Pin )
+        {
+            resetPhysicsBodies()
+        }
+        
+//        let pinned = renderComponent.node.scene!.childNode(withName: kPinnedNode)
+//        let satellite = renderComponent.node.scene!.childNode(withName: kSatelliteNode)
+        
+        //Connect joint to Police nodes
+        let pinned = entity?.component(ofType: RenderComponent.self)?.node
+        let satellite = entityB?.component(ofType: RenderComponent.self)?.node
+        
+        var vz = CGVector(dx: 0, dy: 0)
+        let pinnedAnchor = renderComponent.node.scene!.convert(CGPoint.zero, from: pinned!)
+        let satelliteAnchor = renderComponent.node.scene!.convert(CGPoint.zero, from: satellite!)
+        var joint:SKPhysicsJoint!
+        
+        switch(name)
+        {
+        case .Pin:
+            pinned?.position.y -= ((pinned?.position.y)! - (satellite?.position.y)!) / 2.0
+            resetPhysicsBodies()
+            let pin = SKPhysicsJointPin.joint(withBodyA: (pinned?.physicsBody!)!,
+                                              bodyB: (satellite?.physicsBody!)!,
+                                              anchor: (pinned?.position)!)
+            joint = pin
+        case .Fixed:
+            let fixed = SKPhysicsJointFixed.joint(withBodyA: (pinned?.physicsBody!)!,
+                                                  bodyB: (satellite?.physicsBody!)!,
+                                                  anchor: pinnedAnchor)
+            joint = fixed
+        case JointLabels.Sliding:
+            let sliding = SKPhysicsJointSliding.joint(withBodyA: (pinned?.physicsBody!)!,
+                                                      bodyB: (satellite?.physicsBody!)!,
+                                                      anchor: pinnedAnchor, axis: CGVector(dx: 0, dy: 1))
+            sliding.lowerDistanceLimit = 40
+            sliding.upperDistanceLimit = (pinned?.position.y)! - (satellite?.position.y)!
+            sliding.shouldEnableLimits = true
+            joint = sliding
+        case JointLabels.Spring:
+            let spring = SKPhysicsJointSpring.joint(withBodyA: (pinned?.physicsBody!)!,
+                                                    bodyB: (satellite?.physicsBody!)!,
+                                                    anchorA: pinnedAnchor,
+                                                    anchorB: satelliteAnchor)
+            spring.frequency = 0.5
+            joint = spring
+        case JointLabels.Limit:
+            let limit = SKPhysicsJointLimit.joint(withBodyA: (pinned?.physicsBody!)!,
+                                                  bodyB: (satellite?.physicsBody!)!,
+                                                  anchorA: pinnedAnchor,
+                                                  anchorB: satelliteAnchor)
+            limit.maxLength = 100.0
+            joint = limit
+        }
+        
+        renderComponent.node.scene!.physicsWorld.add(joint)
+        
     }
 }
 
