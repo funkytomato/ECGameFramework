6d912f8ec3148cb4b04639508bbf6f2f89ab950b
Jason Fry <funkytomato@gmail.com>
2018-09-20 17:06:00 +0100

    Comment debug message

--- ECGameFramework/Entities/ProtestorBot.swift
+++ ECGameFramework/Entities/ProtestorBot.swift
@@ -93,7 +93,7 @@ class ProtestorBot: TaskBot, HealthComponentDelegate, ResistanceComponentDelegat
         let randomSource = GKRandomSource.sharedRandom()
         let diff = randomSource.nextUniform() // returns random Float between 0.0 and 1.0
         let speed = diff * GameplayConfiguration.TaskBot.maximumSpeedForIsGood(isGood: isGood) + GameplayConfiguration.TaskBot.minimumSpeed //Ensure it has some speed
-        print("speed :\(speed.debugDescription)")
+//        print("speed :\(speed.debugDescription)")
         
         // Configure the agent's characteristics for the steering physics simulation.
         agent.maxSpeed = speed
