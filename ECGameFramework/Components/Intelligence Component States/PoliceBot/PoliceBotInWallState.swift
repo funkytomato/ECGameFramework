9fad792b0f54d04a4ee9d735351fee50ab72058d
Jason Fry <funkytomato@gmail.com>
2018-09-26 20:45:32 +0100

    Add support for WallComponent. Add check for physicBodies contacts in update method.

--- ECGameFramework/Components/Intelligence Component States/PoliceBot/PoliceBotInWallState.swift
+++ ECGameFramework/Components/Intelligence Component States/PoliceBot/PoliceBotInWallState.swift
@@ -31,6 +31,12 @@ class PoliceBotInWallState: GKState
         return intelligenceComponent
     }
     
+    /// The `WallComponent` associated with the `entity`.
+    var wallComponent: WallComponent
+    {
+        guard let wallComponent = entity.component(ofType: WallComponent.self) else { fatalError("A PoliceBotInWallState entity must have an WallComponent.") }
+        return wallComponent
+    }
     
     
     //MARK:- Initializers
@@ -62,8 +68,61 @@ class PoliceBotInWallState: GKState
         
 //        print("PoliceBotInWallState updating")
 
-        intelligenceComponent.stateMachine.enter(TaskBotAgentControlledState.self)
         
+        //If Police is in RegroupState, connect to wall
+        let currentWallComponentState = wallComponent.stateMachine.currentState
+        if (currentWallComponentState as? RegroupState) != nil
+        {
+            guard let physicsComponent = entity.component(ofType: PhysicsComponent.self) else { return }
+            let contactedBodies = physicsComponent.physicsBody.allContactedBodies()
+            for contactedBody in contactedBodies
+            {
+                guard let entity = contactedBody.node?.entity else { continue }
+                guard let targetBot = entity as? PoliceBot else { break }
+                if self.entity.isPolice && self.entity.connections < 2 /*&& self.entity.requestWall*/ &&
+                    targetBot.isPolice && targetBot.connections < 2
+                {
+                    //Check other PoliceBot is not in wall.
+                    
+                    let policeBotB = entity as? PoliceBot
+                    if /*!policeBotB!.isWall && */policeBotB!.connections < 2
+                    {
+                        
+                        let policeBotBPhysicsComponent = policeBotB?.component(ofType: PhysicsComponent.self)
+                        let policeBRenderComponent = policeBotB?.component(ofType: RenderComponent.self)
+                        let entityB = policeBRenderComponent?.entity
+                        
+                        // Get the Physics Component for each entity
+                        let policeBotA = entity as? PoliceBot
+                        let policeBotAPhysicsComponent = policeBotA?.component(ofType: PhysicsComponent.self)
+                        let policeARenderComponent = policeBotA?.component(ofType: RenderComponent.self)
+                        let entityA = policeARenderComponent?.entity
+                        
+                        
+                        //Connect the two Taskbots together like a rope if forming a wall
+                        guard let intelligenceComponent = self.entity.component(ofType: IntelligenceComponent.self) else { return }
+                        guard ((intelligenceComponent.stateMachine.currentState as? PoliceBotFormWallState) == nil) else { return }
+                        guard let jointComponent = self.entity.component(ofType: JointComponent.self) else { return }
+                        
+                        guard let policeBot = entity as? PoliceBot else { return }
+                        if !jointComponent.isTriggered && policeBot.isPolice
+                        {
+                            jointComponent.setEntityB(targetEntity: policeBotB!)
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+        
+//        intelligenceComponent.stateMachine.enter(TaskBotAgentControlledState.self)
+        
+        if elapsedTime > 60.0
+        {
+            wallComponent.isTriggered = false
+            wallComponent.stateMachine.enter(DisbandState.self)
+        }
         
     }
     
