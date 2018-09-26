d158ebf5c50d5d27e943562c833ebe71ccff78ad
Jason Fry <funkytomato@gmail.com>
2018-09-26 23:42:17 +0100

    Add movement back to TaskBot

--- ECGameFramework/Components/WallComponent (States)/DisbandState.swift
+++ ECGameFramework/Components/WallComponent (States)/DisbandState.swift
@@ -27,6 +27,13 @@ class DisbandState: GKState
     var elapsedTime: TimeInterval = 0.0
     
     
+    /// The `MovementComponent` associated with the `entity`.
+    var movementComponent: MovementComponent
+    {
+        guard let movementComponent = entity.component(ofType: MovementComponent.self) else { fatalError("A DisbandState's entity must have a MovementComponent.") }
+        return movementComponent
+    }
+    
     
     // MARK: Initializers
     
@@ -76,6 +83,21 @@ class DisbandState: GKState
         default:
             return false
         }
+    }
+    
+    override func willExit(to nextState: GKState)
+    {
+        super.willExit(to: nextState)
+        
+        
+        // `movementComponent` is a computed property. Declare a local version so we don't compute it multiple times.
+        let movementComponent = self.movementComponent
+        
+        // Stop the `ManBot`'s movement and restore its standard movement speed.
+        movementComponent.nextRotation = nil
+        movementComponent.nextTranslation = nil
+        movementComponent.movementSpeed /= GameplayConfiguration.TaskBot.movementSpeedMultiplierWhenAttacking
+        movementComponent.angularSpeed /= GameplayConfiguration.TaskBot.angularSpeedMultiplierWhenAttacking
     }
 }
 
