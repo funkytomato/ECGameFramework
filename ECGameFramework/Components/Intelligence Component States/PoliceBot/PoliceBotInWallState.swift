fc14d5deeb5ff11483c6dfa3aab4e20e92be4c3e
Jason Fry <funkytomato@gmail.com>
2018-10-03 21:23:31 +0100

    Fixed issue if two PoliceBots have requested to createWalls.

--- ECGameFramework/Components/Intelligence Component States/PoliceBot/PoliceBotInWallState.swift
+++ ECGameFramework/Components/Intelligence Component States/PoliceBot/PoliceBotInWallState.swift
@@ -118,7 +118,8 @@ class PoliceBotInWallState: GKState
         
 //        intelligenceComponent.stateMachine.enter(TaskBotAgentControlledState.self)
         
-        if !self.entity.requestWall && elapsedTime > 30.0
+//        if !self.entity.requestWall && elapsedTime > 30.0
+        if elapsedTime > 30.0
         {
             wallComponent.isTriggered = false
             wallComponent.stateMachine.enter(DisbandState.self)
