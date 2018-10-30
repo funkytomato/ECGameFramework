74cd192942a74c67ec7edfeea3b44a89963a6a8b
Jason Fry <funkytomato@gmail.com>
2018-10-30 13:37:10 +0000

    Disband after 60 seconds being in a wall.

--- ECGameFramework/Components/WallComponent.swift
+++ ECGameFramework/Components/WallComponent.swift
@@ -134,7 +134,7 @@ class WallComponent: GKComponent
         if policeBot.isWall
         {
             //Police will Disband from wall after 60 seconds whatever
-            if elapsedTime > 30.0
+            if elapsedTime > 60.0
             {
                 stateMachine.enter(DisbandState.self)
             }
